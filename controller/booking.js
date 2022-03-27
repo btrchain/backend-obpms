4a8a1f0f8d8ee9c678d87f79aa2e8fbbb3adbe61
krishna <krishnaqw4141@gmail.com>
2022-03-27 17:52:13 +0530

    sdfof

--- controller/booking.js
+++ controller/booking.js
@@ -1,27 +1,39 @@
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-const Book = require('../model/bookingModel')
-
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const Book = require("../model/bookingModel");
+const sendEmail = require("../utils/email");
 
 exports.book = catchAsync(async (req, res, next) => {
-  
-    // console.log(req.user)
-    // console.log(req.body)
-  
-    const booking = await Book.create({
-     userName: req.user.name,
-     parlourName:req.body.parlourName,
-     productName:req.body.ProductName,
-     date:req.body.date,
-     price:req.body.price,
-     takenTime:req.body.takenTime
-    })
+  // console.log(req.user)
+  // console.log(req.body)
 
+  const booking = await Book.create({
+    userName: req.user.name,
+    parlourName: req.body.parlourName,
+    productName: req.body.ProductName,
+    date: req.body.date,
+    price: req.body.price,
+    takenTime: req.body.takenTime,
+  });
+
+  try {
+    await sendEmail({
+      email: `${req.user.email},${req.body.email}`,
+      subject: "your Booking",
+      message: `${req.body.ProductName}, date: ${req.body.date}
+       price:${req.body.price}, User: ${req.user.name}`,
+    });
     res.status(200).json({
-        data:{
-          status: 'success',
-         booking
-       }
-     })
+      data: {
+        status: "success",
+        booking,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
  
-})
+});
--- controller/product.js
+++ controller/product.js
@@ -1,117 +1,109 @@
-const Product = require('../model/ParlourProduct')
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-const multer = require('multer');
-
+const Product = require("../model/ParlourProduct");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const multer = require("multer");
 
 // const upload = multer({dest:'public/image'})
 
 const multerStorage = multer.diskStorage({
-  destination:(req,file,cb)=>{
-    cb(null,'public/img/')
+  destination: (req, file, cb) => {
+    cb(null, "public/img/");
   },
-  filename:(req,file,cb)=>{
+  filename: (req, file, cb) => {
     //user-684845df5fd-timestamp.extension
-    const ext = file.mimetype.split('/')[1];
-    cb(null,`service-${req.parlour.id}-${Date.now()}.${ext}`)
-  }
-})
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `service-${req.parlour.id}-${Date.now()}.${ext}`);
+  },
+});
 
-const multerFilter=(req,file,cb)=>{
-  if (file.mimetype.startsWith('image')) {
-    cb(null,true)
-  }else{
-    cb(new AppError('Not an Image ! please upload only image',400),false)
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an Image ! please upload only image", 400), false);
   }
-}
+};
 
 const upload = multer({
-  storage:multerStorage,
-  fileFilter:multerFilter
-})
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
 
-exports.uploadImage=upload.single('photo')
-
+exports.uploadImage = upload.single("photo");
 
 exports.addproduct = catchAsync(async (req, res, next) => {
   // console.log(req.file,req.parlour)
-  let photourl = `${req.protocol}://${req.get('host')}/img/${req.file.filename}`
+  let photourl = `${req.protocol}://${req.get("host")}/img/${
+    req.file.filename
+  }`;
   // console.log(photourl)
 
   const products = await Product.create({
-  title:req.body.title,
-  photo:photourl,
-  desc:req.body.desc,
-  price:req.body.price,
-  parlour:req.parlour.id,
-  duration:req.body.duration,
-  })
-  
+    title: req.body.title,
+    photo: photourl,
+    desc: req.body.desc,
+    price: req.body.price,
+    parlour: req.parlour.id,
+    duration: req.body.duration,
+  });
+
   // console.log(products)
- res.status(200).json(products)
+  res.status(200).json(products);
+});
 
-})
-
-
-
-
-
-exports.getProduct= catchAsync(async (req, res, next) => {
- const products = await Product.find({parlour:req.parlour.id}).select('-parlour')
+exports.getProduct = catchAsync(async (req, res, next) => {
+  const products = await Product.find({ parlour: req.parlour.id }).select(
+    "-parlour"
+  );
   // console.log(products)
   if (!products) {
-    return next(new AppError('you are not provided services yet',404,'faild'))
+    return next(
+      new AppError("you are not provided services yet", 404, "faild")
+    );
   }
-  
+
   res.status(200).json({
-    data:{
-      status: 'success',
-      products
- }
-  })
-   
-})
+    data: {
+      status: "success",
+      products,
+    },
+  });
+});
 
-
- 
 exports.getAllProducts = catchAsync(async (req, res, next) => {
- 
   const allProducts = await Product.find({}).populate({
-    path:'parlour',
-    select:'name'
-  })
+    path: "parlour",
+    select: "name",
+  });
 
   if (!allProducts) {
-    return next(new AppError('no any product available yet',404,'faild'))
+    return next(new AppError("no any product available yet", 404, "faild"));
   }
 
   res.status(200).json({
-    data:{
-      status: 'success',
-      allProducts
-   }
- })
-
-})
-
+    data: {
+      status: "success",
+      allProducts,
+    },
+  });
+});
 
 exports.getProductbyId = catchAsync(async (req, res, next) => {
-
   // console.log(req.params.id)
   const allProducts = await Product.findById(req.params.id).populate({
-    path:'parlour',
-    select:'name localityName cityName pinCode email'
-  })
+    path: "parlour",
+    select: "name localityName cityName pinCode email",
+  });
   if (!allProducts) {
-    return next(new AppError('product not found please try again',404,'failed'))
+    return next(
+      new AppError("product not found please try again", 404, "failed")
+    );
   }
   // console.log(allProducts)
   res.status(200).json({
-    data:{
-      status: 'success',
-      allProducts
-   }
- })
-})
-
-
+    data: {
+      status: "success",
+      allProducts,
+    },
+  });
+});
