114f9f25b48ac630587ae0e4ce573e7eeaadc9de
krishna <krishnaqw4141@gmail.com>
2022-03-27 13:58:37 +0530

    lsdfj

--- controller/booking.js
+++ controller/booking.js
@@ -5,22 +5,23 @@ const Book = require('../model/bookingModel')
 
 exports.book = catchAsync(async (req, res, next) => {
   
-    console.log(req.user)
-    console.log(req.body)
+    // console.log(req.user)
+    // console.log(req.body)
   
-    // const booking = await Book.create({
-    //  userName: req.user.name,
-    //  parlourName:req.body.parlourName,
-    //  productName:req.body.ProductName,
-    //  date:req.body.date,
-    //  price:req.body.price
-    // })
+    const booking = await Book.create({
+     userName: req.user.name,
+     parlourName:req.body.parlourName,
+     productName:req.body.ProductName,
+     date:req.body.date,
+     price:req.body.price,
+     takenTime:req.body.takenTime
+    })
 
-    // res.status(200).json({
-    //     data:{
-    //       status: 'success',
-    //      booking
-    //    }
-    //  })
+    res.status(200).json({
+        data:{
+          status: 'success',
+         booking
+       }
+     })
  
 })
