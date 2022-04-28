a3ccb389172d02a2966865cd6ae771beab367c36
krishna <krishnaqw4141@gmail.com>
2022-04-28 20:15:20 +0530

    dfsd

--- route/parlourRoute.js
+++ route/parlourRoute.js
@@ -29,7 +29,7 @@ router.route('/today').post(book.toDayAppointment)
 
 router.route('/allproducts').get(product.getAllProducts)
 router.route('/productadd').post(auth.protect,product.uploadImage ,product.addproduct)
-router.route('/productedit/:id').patch(auth.protect,product.uploadImage ,product.productEdit)
+router.route('/productedit/:id').patch(auth.protect,product.productEdit)
 router.route('/productdelete/:id').delete(auth.protect,product.productDelete)
 router.route('/productget').get(auth.protect,product.getProduct)
 router.route('/getAllParlours').post(auth.searchParlours)
