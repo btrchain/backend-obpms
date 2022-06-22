1c3e703abfab415af77ff917a89262aeff97785e
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2022-06-23 01:05:55 +0530

    fix

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use(express.json())
 app.use( cors())
 
 mongoose.connect(
-    // process.env.MONGO_DB,
-    process.env.MONGO_DB_lOCALHOST,
+    process.env.MONGO_DB,    //uncomment if using remote db
+    // process.env.MONGO_DB_lOCALHOST, //uncomment if using local db
     {
         useNewUrlParser: true,
         // useUnifiedTopology: true
